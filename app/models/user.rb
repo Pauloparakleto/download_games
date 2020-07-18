dc90fe587569f1d16c2d0fd8c7ebfff40d21d50e
Pauloparakleto <alparakleto@gmail.com>
2020-07-18 18:32:49 -0300

    Add  passwords function to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,13 @@
 class User < ApplicationRecord
     has_secure_password
     has_secure_password :recovery_password, validations: false
+
+    def password
+       @password ||= Password.new(password_hash)
+    end
+    
+    def password=(new_password)
+       @password = Password.create(new_password)
+       self.password_hash = @password
+    end
 end
