991a3e5310ca55a044caad23ea7e2a05f0ca1147
Pauloparakleto <alparakleto@gmail.com>
2020-07-18 18:18:04 -0300

    create User table with password_digest and Ppassword_digest_recovery

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_16_030001) do
+ActiveRecord::Schema.define(version: 2020_07_18_210519) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,14 @@ ActiveRecord::Schema.define(version: 2020_07_16_030001) do
   create_table "roms", force: :cascade do |t|
     t.string "name"
     t.string "image_url"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "password_digest"
+    t.string "recovery_password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
