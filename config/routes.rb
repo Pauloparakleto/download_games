7fc6e3e470c3ddd30cb0d6db022caddd383f715c
Pauloparakleto <alparakleto@gmail.com>
2020-07-21 03:13:25 -0300

    Add get login and post login to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   get 'login', to: 'sessions#new', as: 'login'
+  post 'login', to: 'sessions#create'
   get 'signup', to: 'user#new', as: 'signup'
   post 'signup', to: 'user#create'
-  root to: 'admin_home', to: 'roms#index', as: 'admin_home'
+  root to: 'admin', to: 'roms#index', as: 'admin_home'
   get 'download/:id', to: 'roms#download', as: 'download'
   resources :roms
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
