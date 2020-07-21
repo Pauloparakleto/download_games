6dc0f449129cfa11b532e0b013f353cee6fdc664
Pauloparakleto <alparakleto@gmail.com>
2020-07-21 03:11:59 -0300

    Authenticate user. Obs: there is a duplication of flash message on view

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,15 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    if User.find_by(name: params[:name]).try(:authenticate, params[:password])
+      redirect_to admin_home_path
+      flash[:notice] = 'Successfully loged in!'
+      p "Helooooooooow"
+    else
+      flash[:alert] = 'Invalid username or password!'
+      redirect_to login_path
+    end
+  end
 end
