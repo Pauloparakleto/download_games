691d817d5d67d383e748614148475cacc7fa1802
Pauloparakleto <alparakleto@gmail.com>
2020-07-22 15:11:59 -0300

    Add destroy logout function to sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -20,7 +20,7 @@ class SessionsController < ApplicationController
   def destroy
     session[:user_id] = nil
     session[:user_name] = nil
-    flash[:success] = "Successfully Logged Out!"
-    redirect_to '/login'
+    flash[:notice] = "Successfully Logged Out!"
+    redirect_to login_path
    end
 end
