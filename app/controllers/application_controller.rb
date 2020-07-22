d416cbb0b05eb2a7d691f4eb17ba16fe16e7c2da
Pauloparakleto <alparakleto@gmail.com>
2020-07-22 14:56:17 -0300

    Add current_user and authenticate_user test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+    
+    def current_user
+        return @current_user = session[:user_id] if session[:user_id]
+    end
+    def authenticate_use
+    end
 end
