c5d4d6f67cd6993ffd222099feeb36b8fc03f8d3
Pauloparakleto <alparakleto@gmail.com>
2020-07-22 15:12:35 -0300

    Add logout routes to sessions destroy

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'login', to: 'sessions#new', as: 'login'
   post 'login', to: 'sessions#create'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
   get 'signup', to: 'user#new', as: 'signup'
   post 'signup', to: 'user#create'
   root to: 'admin', to: 'roms#index', as: 'admin_home'
