14767dc4dc6cee36f09a316d648f9f0ad9d76469
Pauloparakleto <alparakleto@gmail.com>
2020-07-25 00:27:28 -0300

    Migration scheema to add zip_file_link to rom table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_18_210519) do
+ActiveRecord::Schema.define(version: 2020_07_25_001011) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 2020_07_18_210519) do
     t.string "image_url"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "zip_file_link"
   end
 
   create_table "users", force: :cascade do |t|
